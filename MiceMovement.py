9c8a5a11dd0f551ebdcc3e3c6058d2b620997c38
Verdi <152645717+tfVerdi@users.noreply.github.com>
2024-10-09 20:23:12 -0300

    First commit

---
+++ MiceMovement.py
@@ -0,0 +1,43 @@
+import asyncio
+from keyboard import is_pressed, press, release
+from mouse import move
+from random import randint
+
+# Setup config
+minimo_espera = 40
+maximo_espera = 60
+tab_switch = True
+
+async def pressAndRelease(key: str):
+    press(key)
+    await asyncio.sleep(0.05)
+    release(key)
+    await asyncio.sleep(0.05)
+
+async def activitySim(tab_switch: bool = tab_switch):
+    if tab_switch:
+        await pressAndRelease('alt+tab')
+        await pressAndRelease('alt+tab')
+    move(randint(600, 1200), randint(0, 800), duration=0.25)
+    
+async def checkForQuit():
+    if is_pressed('esc'):
+        print("Has detenido el programa.")
+        print("Ya puedes cerrar esta ventana, pero se cerrará de todos modos en unos segundos.")
+        return True
+    return False
+
+async def mainLoop():
+    stop = False
+    while not stop:
+        await activitySim()
+
+        wait_time = randint(minimo_espera, maximo_espera)
+        for _ in range(wait_time*4):
+            await asyncio.sleep(0.25)
+            if await checkForQuit():
+                stop = True
+                break
+
+print("El programa empezará de inmediato...\n\nPara detener el programa cierra esta ventana o mantén presionado Esc.\n")
+asyncio.run(mainLoop())
---
+++ README.md
@@ -0,0 +1,14 @@
+# PC Idler
+
+There are 2 versions of this program, "MouseMovement_wTabSwitch.exe" and "MouseMovement.exe"
+
+"MouseMovement.exe":
+Moves the mouse in an interval of 40-60 seconds, making the computer register mouse movement.
+
+"MouseMovement_wTabSwitch.exe":
+Moves the mouse, but also uses the Alt+Tab function, making the computer register mouse movement and keyboard keys being pressed. If "MouseMovement.exe" isn't enough for what you're trying to use it for, try this version out.
+
+*** Alt+tab is a shortcut to bring forward the last window, it does not type, nor does it leave any permanent changes. All it does is change the focus from one open app to another.
+
+
+- Verdi
